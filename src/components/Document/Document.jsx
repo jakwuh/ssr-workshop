0812537aef2397d632896d7fd3028710b72f49d3
jakwuh <jakwuh@gmail.com>
2018-09-08 19:07:12 +0300

    2.3. Render empty document

---
+++ src/components/Document/Document.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const style = `
+body {
+    font-family: 'Helvetica Neue', Arial, sans-serif;
+    color: #333;
+    font-weight: 300;
+}
+`;
+
+export default () => <>
+    <html>
+    <head lang='en'>
+        <meta charSet='utf-8'/>
+        <meta name='viewport' content='width=device-width'/>
+        <title>SSR Workshop</title>
+        <style dangerouslySetInnerHTML={{ __html: style }} />
+    </head>
+    <body>
+    <div id="root"/>
+    <script src='/bundle.js'/>
+    </body>
+    </html>
+</>
--- src/entries/server.jsx
+++ src/entries/server.jsx
@@ -1,9 +1,14 @@
+import React from 'react';
 import Koa from 'koa';
+import Document from '../components/Document/Document';
+import { renderToString } from 'react-dom/server';
 
+const docType = '<!doctype html>';
 const app = new Koa();
 
 app.use(async ctx => {
-    ctx.body = 'Hello World';
+    const document = <Document />;
+    ctx.body = `${docType}${renderToString(document)}`;
 });
 
 app.listen(3000);
